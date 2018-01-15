f34348f7e99e64e8085c48c4fff6dc891eb1e03a
Michael MacLeod <michaelmmacleod@gmail.com>
2018-01-15 11:47:10 -0800

    Change the List type into a Tree type

---
+++ types/tree/tree.h
@@ -0,0 +1,29 @@
+/* tree.h
+ *
+ * Defines the Tree type.
+ *
+ * includes
+ *      "../node/node.h"
+ *
+ * typedefs
+ *      Tree
+ */
+
+#ifndef INCLUDE_TREE_H
+#define INCLUDE_TREE_H
+
+#include "../node/node.h"
+
+/* A Tree of Nodes.
+ *
+ * ex:
+ *      Tree t;
+ *      t.node = node;
+ *      t.branches[0] = &child0;
+ */
+typedef struct {
+    Node node;
+    Node* branches;
+} Tree;
+
+#endif
