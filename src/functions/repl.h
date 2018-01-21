c8c000464f400ccc38070c3ea80871842ed1d737
Michael MacLeod <michaelmmacleod@gmail.com>
2018-01-20 16:11:27 -0800

    Fix some memory leaks reported by valgrind

--- src/functions/repl.h
+++ src/functions/repl.h
@@ -15,13 +15,19 @@ struct Atom *read() {
     int nchars = getline(&buffer, &bsize, stdin);
 
     if (nchars == 1) {
+        free(buffer);
+
         printf(" ==> exit\n");
         exit(0);
     }
 
     buffer[nchars - 1] = '\0'; // remove '\n'
 
-    return read_atom(buffer);
+    struct Atom *atom = read_atom(buffer);
+
+    free(buffer);
+
+    return atom;
 }
 
 struct Atom *eval(struct Atom *atom) {
@@ -32,6 +38,8 @@ void print(struct Atom *atom) {
     printf(" ==> ");
     print_atom(atom);
     printf("\n");
+
+    free_atom(atom);
 }
 
 #endif
