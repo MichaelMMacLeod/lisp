d67600060e0ca3d78ecf6ee6f73d1cf5198bdd4b
Michael MacLeod <michaelmmacleod@gmail.com>
2018-03-29 19:37:37 -0700

    Add map.h
    Working towards making the environment and readtable first class
    citizens in the language so we can have macros and the like. This
    means redefining struct env as a more general map (we don't want to
    be defining a readtable as an env, right?)

---
+++ map.h
@@ -0,0 +1,107 @@
+#ifndef INCLUDE_MAP_H
+#define INCLUDE_MAP_H
+
+#include <stdlib.h>
+#include <string.h>
+#include "sexpr.h"
+
+// create_item - create an item with a key and value
+//
+// return - the new item
+struct item *create_item(char *key, struct sexpr *value) {
+    struct item *result = malloc(sizeof(struct item));
+
+    result->key = key;
+    result->value = value;
+
+    return result;
+}
+
+// create_empty_map - create a map of size zero
+// 
+// return - the empty map
+struct map *create_empty_map() {
+    struct map *result = malloc(sizeof(struct map));
+
+    result->size = 0;
+    result->items = NULL;
+
+    return result;
+}
+
+// get - return the item in a map with a key lexicographically equal to the one
+// provided
+//
+// return - the item, or NULL if it doesn't exist in the map
+struct item *get(char *key, struct map *m) {
+    for (int k = 0; k < m->size; ++k) {
+        if (strcmp(key, m->items[k].key) == 0) {
+            return &m->items[k];
+        }
+    }
+
+    return NULL;
+}
+
+// add_non_shadowing - add an item to a map but don't shadow an existing one if
+// it exists
+//
+// return - the new item, or, if it was a duplicate, the original
+struct item *add_non_shadowing(struct item *i, struct map *m) {
+    struct item *possible_duplicate = get(i->key, m);
+
+    if (possible_duplicate == NULL) {
+        ++m->size;
+
+        m->items = realloc(m->items, m->size * sizeof(struct item));
+        m->items[m->size - 1] = *i;
+
+        return i;
+    } else {
+        return possible_duplicate;
+    }
+}
+
+// add_shadowing - add an item to a map and shadow an existing one if it exists
+//
+// return - the new item
+struct item *add_shadowing(struct item *i, struct map *m) {
+    struct item *possible_duplicate = get(i->key, m);
+
+    if (possible_duplicate == NULL) {
+        ++m->size;
+
+        m->items = realloc(m->items, m->size * sizeof(struct item));
+        m->items[m->size - 1] = *i;
+
+        return i;
+    } else {
+        possible_duplicate->value = i->value;
+
+        return possible_duplicate;
+    }
+}
+
+// remove_item - remove an item from a map
+//
+// return - the removed item, NULL if it didn't exist in the map
+struct item *remove_item(struct item *i, struct map *m) {
+    for (int j = 0; j < m->size; ++j) {
+        if (strcmp(m->items[j].key, i->key) == 0) {
+            struct item *removed = &m->items[j];
+            struct item *result = create_item(removed->key, removed->value);
+
+            for (int k = j; k < m->size - 1; ++k) {
+                m->items[k] = m->items[k + 1];
+            }
+
+            --m->size;
+
+            return result;
+        }
+    }
+
+    return NULL;
+}
+
+#endif
--- sexpr.h
+++ sexpr.h
@@ -2,11 +2,12 @@
 #define INCLUDE_SEXPR_H
 
 struct sexpr {
-    enum { SYMBOL, PAIR, FUNCTION } type;
+    enum { SYMBOL, PAIR, FUNCTION, MAP } type;
     union {
         char *symbol;
         struct pair *pair;
         struct function *function;
+        struct map *map;
     };
 };
 
@@ -18,6 +19,16 @@ struct pair {
 struct function {
     struct pair *args;
     struct sexpr *body;
+};
+
+struct map {
+    int size;
+    struct item *items;
+};
+
+struct item {
+    char *key;
+    struct sexpr *value;
 };
 
 #endif
