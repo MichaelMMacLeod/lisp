49aa4d209cc79eae04f05d3a8c15b8d77887bef5
Michael MacLeod <michaelmmacleod@gmail.com>
2018-04-02 18:06:51 -0700

    Make t select first, nil select second

--- src/functions/frontend/nil.h
+++ src/functions/frontend/nil.h
@@ -2,11 +2,11 @@
 #define INCLUDE_FRONTEND_NIL_H
 
 struct sexpr *nil(struct list *args, struct map *package) {
-    struct sexpr *result = malloc(sizeof(struct sexpr));
-    result->type = SYMBOL;
-    result->symbol = get("NIL", package)->key;
+    if (args->tail != NULL) {
+        return eval_sexpr(args->tail->head, package);
+    }
 
-    return result;
+    return NULL;
 }
 
 #endif
